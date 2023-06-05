7f4d3d96a414e308cc75ef0a6e01d2bf00e94ea7
gwangseok.ha <laneli1@naver.com>
2023-06-05 12:22:23 +0900

    First Commit

---
+++ SocketTest/src/test/java/com/example/sockettest/SocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
